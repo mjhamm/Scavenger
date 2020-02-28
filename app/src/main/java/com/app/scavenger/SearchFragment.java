2cddc05e3f9b6b82ab916fd7649a6e5595348724
Michael <mjhamm@bu.edu>
2020-02-28 11:11:25 -0500

    2/28/2020 Updated search fragment

--- app/src/main/java/com/app/scavenger/SearchFragment.java
+++ app/src/main/java/com/app/scavenger/SearchFragment.java
@@ -1,6 +1,8 @@
 package com.app.scavenger;
 
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -14,9 +16,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
+import java.util.Random;
 
 import static android.view.View.GONE;
 
@@ -26,6 +32,8 @@ public class SearchFragment extends Fragment {
     private ArrayList<RecipeItem> recipeItems;
     private SearchAdapter adapter;
     private Context mContext;
+    private SearchView mSearchView;
+    private TextView startup_message;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -41,11 +49,35 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
 
-        SearchView mSearchView = view.findViewById(R.id.search_searchView);
+        startup_message = view.findViewById(R.id.startup_message);
+        mSearchView = view.findViewById(R.id.search_searchView);
         mSearchView.setMaxWidth(Integer.MAX_VALUE);
 
-        //recipeItems = new ArrayList<>();
-        recipeItems = RecipeItem.createContactsList(20);
+        Random random_start_number = new Random();
+
+        //creates a random number and sets the welcome text to a specific text based on number
+        int start_message_int = random_start_number.nextInt(5);
+
+        switch (start_message_int) {
+            case 0:
+                startup_message.setText("Scavenge For Recipes!");
+                break;
+            case 1:
+                startup_message.setText("Find Recipes You'll Love!");
+                break;
+            case 2:
+                startup_message.setText("Add Up All The\nThyme You Can Save!");
+                break;
+            case 3:
+                startup_message.setText("Break An Egg, Make An Omelet!");
+                break;
+            case 4:
+                startup_message.setText("Check The Way Back Of The Cabinet!");
+                break;
+        }
+
+        recipeItems = new ArrayList<>();
+        //recipeItems = RecipeItem.createContactsList(20);
 
         mSearchRecyclerView = view.findViewById(R.id.search_recyclerView);
         mSearchRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
@@ -55,5 +87,22 @@ public class SearchFragment extends Fragment {
         mSearchRecyclerView.setAdapter(adapter);
 
         return view;
+    }
+
+    //Gets the input from Searchview and returns it as string
+    private String getIngredientsSearch() {
+        return mSearchView.getQuery().toString();
+    }
+
+    //boolean that returns true if you are connected to internet and false if not
+    private boolean checkConnection() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnected();
+    }
+
+    //method for creating a Toast
+    private void toastMessage(String message) {
+        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
     }
 }
