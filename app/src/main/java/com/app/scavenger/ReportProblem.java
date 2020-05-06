fe93150e9f015caf4996e43fa9bd2c01d688bda3
Michael <mjhamm@bu.edu>
2020-05-06 19:23:41 -0400

    5/6/2020 Updates to feedback and report

--- app/src/main/java/com/app/scavenger/ReportProblem.java
+++ app/src/main/java/com/app/scavenger/ReportProblem.java
@@ -3,12 +3,53 @@ package com.app.scavenger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.button.MaterialButton;
 
 public class ReportProblem extends AppCompatActivity {
+
+    private EditText reportEditText;
+    private MaterialButton reportSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_problem);
+
+        reportEditText = findViewById(R.id.report_editText);
+        reportSubmitButton = findViewById(R.id.report_submit);
+
+        reportSubmitButton.setEnabled(false);
+        reportSubmitButton.setTextColor(getResources().getColor(R.color.dark_gray, null));
+        reportSubmitButton.setBackgroundColor(getResources().getColor(android.R.color.white, null));
+
+        reportEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().trim().length() != 0) {
+                    reportSubmitButton.setEnabled(true);
+                    reportSubmitButton.setTextColor(getResources().getColor(android.R.color.white, null));
+                    reportSubmitButton.setBackgroundColor(getResources().getColor(R.color.buttonRed, null));
+                } else {
+                    reportSubmitButton.setEnabled(false);
+                    reportSubmitButton.setTextColor(getResources().getColor(R.color.dark_gray, null));
+                    reportSubmitButton.setBackgroundColor(getResources().getColor(android.R.color.white, null));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/app/scavenger/SendFeedback.java
+++ app/src/main/java/com/app/scavenger/SendFeedback.java
@@ -3,12 +3,52 @@ package com.app.scavenger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import com.google.android.material.button.MaterialButton;
 
 public class SendFeedback extends AppCompatActivity {
+
+    private EditText feedbackEditText;
+    private MaterialButton feedbackSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_feedback);
+
+        feedbackEditText = findViewById(R.id.feedback_editText);
+        feedbackSubmitButton = findViewById(R.id.feedback_submit);
+
+        feedbackSubmitButton.setEnabled(false);
+        feedbackSubmitButton.setTextColor(getResources().getColor(R.color.dark_gray, null));
+        feedbackSubmitButton.setBackgroundColor(getResources().getColor(android.R.color.white, null));
+
+        feedbackEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.toString().trim().length() != 0) {
+                    feedbackSubmitButton.setEnabled(true);
+                    feedbackSubmitButton.setTextColor(getResources().getColor(android.R.color.white, null));
+                    feedbackSubmitButton.setBackgroundColor(getResources().getColor(R.color.buttonRed, null));
+                } else {
+                    feedbackSubmitButton.setEnabled(false);
+                    feedbackSubmitButton.setTextColor(getResources().getColor(R.color.dark_gray, null));
+                    feedbackSubmitButton.setBackgroundColor(getResources().getColor(android.R.color.white, null));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
